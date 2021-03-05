d536f96d3745a9ddea9963b9a56b7a5dae4eb4ca
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2021-03-05 17:48:19 +0000

    userController added

---
+++ JmbpPrintAPI/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JmbpPrintAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityJwtAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        [HttpGet("userslist")]
+        public async Task<IActionResult> getAllUsersAsync()
+        {
+            var users = await _db.Users.ToListAsync();
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+    }
+}
--- JmbpPrintAPI/Models/ApplicationDbContext.cs
+++ JmbpPrintAPI/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IdentityJwtAPIDemo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,5 +16,6 @@ namespace JmbpPrintAPI.Models
         }
 
         public DbSet<Employee> Employees { get; set; }
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
